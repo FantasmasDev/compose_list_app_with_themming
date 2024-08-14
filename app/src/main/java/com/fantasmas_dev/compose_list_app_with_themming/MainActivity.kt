dc4a88f2519d3c684e19a0dfd859926406c32f70
FantasmasDe <fantasmas.dev@gmail.com>
2024-08-14 22:27:12 +0300

    Доработка дизайна приложения

--- app/src/main/java/com/fantasmas_dev/compose_list_app_with_themming/MainActivity.kt
+++ app/src/main/java/com/fantasmas_dev/compose_list_app_with_themming/MainActivity.kt
@@ -9,12 +9,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -23,34 +30,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fantasmas_dev.compose_list_app_with_themming.data.Dog
-import com.fantasmas_dev.compose_list_app_with_themming.ui.theme.Compose_list_app_with_themmingTheme
+import com.fantasmas_dev.compose_list_app_with_themming.data.dogs
+import com.fantasmas_dev.compose_list_app_with_themming.ui.theme.Compose_list_app_with_theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Compose_list_app_with_themmingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-                        Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                    }
-                }
+            Compose_list_app_with_theme {
+                DogsApp()
             }
         }
     }
@@ -60,7 +57,9 @@ class MainActivity : ComponentActivity() {
 fun AppHeader(modifier: Modifier = Modifier) {
     Row(
         modifier = modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+        ,
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -76,26 +75,6 @@ fun AppHeader(modifier: Modifier = Modifier) {
     }
 }
 
-//fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
-//    Card(modifier = modifier) {
-//        Column {
-//            Image(
-//                painter = painterResource(id = affirmation.imagerResourceId),
-//                contentDescription = stringResource(id = affirmation.stringResourceId),
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(194.dp),
-//                contentScale = ContentScale.Crop
-//            )
-//            Text(
-//                text = LocalContext.current.getString(affirmation.stringResourceId),
-//                modifier = Modifier.padding(16.dp),
-//                style = MaterialTheme.typography.headlineSmall
-//            )
-//        }
-//    }
-//}
-
 @Composable
 fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
     Card(
@@ -105,6 +84,7 @@ fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
                 start = 8.dp,
                 end = 8.dp
             )
+            .clip(MaterialTheme.shapes.medium)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically
@@ -115,12 +95,10 @@ fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
                     contentDescription = stringResource(id = dog.name),
                     modifier = Modifier
                         .size(width = 68.dp, height = 68.dp)
-                        .aspectRatio(1f)
-                        .padding(start = 8.dp),
+                        .padding(start = 8.dp)
+                        .clip(MaterialTheme.shapes.small),
                     contentScale = ContentScale.Crop
                 )
-
-
             }
             Column(
                 modifier = Modifier
@@ -129,7 +107,7 @@ fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
                         end = 16.dp,
                         start = 16.dp,
                         bottom = 16.dp
-                    )
+                    ),
             ) {
                 Text(
                     text = stringResource(id = dog.name),
@@ -139,9 +117,6 @@ fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
                     text = dog.age.toString(),
                     maxLines = 1,
                     modifier = Modifier
-                        .padding(
-                            top = 16.dp,
-                        )
                 )
             }
         }
@@ -151,9 +126,43 @@ fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    AppHeader()
-    DogCard(dog = Dog(R.drawable.koda, R.string.dog_name_1, 2, R.string.dog_description_1))
+fun DogsList(dogList: List<Dog>, modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = { AppHeader() }
+    ) {
+        padding ->
+        LazyColumn(modifier = modifier, contentPadding = padding) {
+        items(dogList) { dog ->
+            DogCard(
+                dog = dog,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+    }
+}
+
+
+@Composable
+fun DogsApp() {
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateEndPadding(layoutDirection)
+            )
+    ) {
+        Column {
+            DogsList(dogList = dogs)
+        }
+    }
 }
 
 @Preview(
@@ -161,8 +170,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     showSystemUi = true
 )
 @Composable
-fun GreetingPreview() {
-    Compose_list_app_with_themmingTheme {
-        Greeting("Android")
+fun AppPreview() {
+    Compose_list_app_with_theme {
+        DogsApp()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun DarkThemeAppPreview() {
+    Compose_list_app_with_theme(darkTheme = true) {
+        DogsApp()
     }
 }
---
+++ app/src/main/java/com/fantasmas_dev/compose_list_app_with_themming/ui/theme/Shapes.kt
@@ -0,0 +1,10 @@
+package com.fantasmas_dev.compose_list_app_with_themming.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
+)
